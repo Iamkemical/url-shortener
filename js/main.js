20423face39a754c3684a91763f952ee22183d48
Gabriel Isaac <iamkemical1@gmail.com>
2025-04-26 08:48:31 +0100

    feat: Enhance footer layout and add section headings; improve mobile menu functionality

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hamburgerButton = document.querySelector(".hamburger-button");
   const mobileMenu = document.querySelector(".mobile-menu");
+  const hamburgerLine = document.querySelectorAll(".hamburger-line");
 
   hamburgerButton.addEventListener("click", () => {
     mobileMenu.classList.toggle("active");
+    hamburgerLine.forEach((line) => line.classList.toggle("disabled"));
   });
 });
